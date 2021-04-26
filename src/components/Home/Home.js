7606dfc087a9f47fd57b7a54d7c8294ea681b351
Daniyal Kaleem <cobaltdaniyal@outlook.com>
2021-04-26 23:45:45 +0500

    carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,51 +2,60 @@ import React, { Component } from 'react';
 import './Home.css';
 import Navbar from '../Navbar/Navbar';
 import { Carousel } from 'react-bootstrap';
+import slider1 from '../../assets/images/slider1.jpg'
+import slider2 from '../../assets/images/slider2.jpg'
+import slider3 from '../../assets/images/slider3.jpg'
+import slider4 from '../../assets/images/slider4.jpg'
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Navbar />
+
+                {/* <<<<<<<<<<<<<<<<<<<<<<<<<< CAROUSEL START >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
                 <div className='carouselMain'>
-                    <Carousel>
+                    <Carousel nextIcon >
                         <Carousel.Item>
                             <img
                                 className="d-block w-100"
-                                src="https://i.ytimg.com/vi/go89e8xpVYs/maxresdefault.jpg"
+                                src={slider1}
                                 alt="First slide"
                             />
-                            <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
                         </Carousel.Item>
+
                         <Carousel.Item>
                             <img
                                 className="d-block w-100"
-                                src="https://www.thesprucecrafts.com/thmb/iLiJofm6fgoH1cME7PSH2UDyo4w=/1339x1339/smart/filters:no_upscale()/15Stitches-587161e53df78c17b6e270e8.jpg"
+                                src={slider2}
                                 alt="Second slide"
                             />
-
-                            <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
                         </Carousel.Item>
+
                         <Carousel.Item>
                             <img
                                 className="d-block w-100"
-                                src="https://gulahmed.com/wp/wp-content/uploads/2016/03/3.jpg"
+                                src={slider3}
                                 alt="Third slide"
                             />
-
-                            <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </Carousel.Caption>
                         </Carousel.Item>
+
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={slider4}
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+
                     </Carousel>
                 </div>
+                {/* <<<<<<<<<<<<<<<<<<<<<<<<<< CAROUSEL END >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+
+                <div>
+                    <h1>asdasdasds</h1>
+                </div>
+
             </div>
         )
     }
