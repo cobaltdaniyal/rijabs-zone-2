547809a284fc0ab823ddeb2ac3acc3be1658ec27
Daniyal Kaleem <cobaltdaniyal@outlook.com>
2021-04-29 22:35:53 +0500

    firebase

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../../assets/images/logo.png';
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import firebase from '../../config/firebase';
+import firebase from '../../config/firebse';
 
 class Navbar extends Component {
     state = {
