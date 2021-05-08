da89f4fb25759823698f4573d2ec3d0c014ca5b4
Daniyal Kaleem <cobaltdaniyal@outlook.com>
2021-05-08 22:35:17 +0500

    category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -10,12 +10,15 @@ class Category extends Component {
                 <h1 className='mainHeading'>Categories</h1>
                 <div className='categorySection'>
                     <Card className='cat' body>Men's Fashion</Card>
+                    <Card className='cat' body>Men's Watches</Card>
                     <Card className='cat' body>Men's Accessories</Card>
                     <Card className='cat' body>Women's Fashion</Card>
+                    <Card className='cat' body>Women's Watches</Card>
                     <Card className='cat' body>Women's Jwellery</Card>
                     <Card className='cat' body>Women's Accessories</Card>
                     <Card className='cat' body>Babies's Fashion</Card>
                     <Card className='cat' body>Babies's Accessories</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
                 </div>
             </div>
         )
