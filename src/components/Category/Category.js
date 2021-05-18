7bc33c45e6793ba7e059685ce9675aa7d01439c1
Daniyal Kaleem <cobaltdaniyal@outlook.com>
2021-05-18 21:22:35 +0500

    category

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -22,6 +22,13 @@ class Category extends Component {
                     <Card className='cat' body>Babies's Fashion</Card>
                     <Card className='cat' body>Babies's Accessories</Card>
                     <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
+                    <Card className='cat' body>Babies's Toys</Card>
                 </div>
             </div>
         )
